edc45d20bbe705d3b54c6a6d906f2d17bac9327b
Nazli Bozoglu <nzlbozoglu@gmail.com>
2020-10-05 20:32:32 -0400

    initial commit

---
+++ src/default-board.js
@@ -0,0 +1,52 @@
+import { uuid } from './utils'
+
+export default {
+  name: 'PacePoint',
+  columns: [
+    {
+      name: 'TODO',
+      tasks: [
+        {
+          description: '',
+          name: 'first task',
+          id: uuid(),
+          userAssigned: null
+        },
+        {
+          description: '',
+          name: 'second task',
+          id: uuid(),
+          userAssigned: null
+        },
+        {
+          description: '',
+          name: 'and third',
+          id: uuid(),
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: 'In Progress',
+      tasks: [
+        {
+          description: '',
+          name: 'first task',
+          id: uuid(),
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: 'Done',
+      tasks: [
+        {
+          description: '',
+          name: 'first task',
+          id: uuid(),
+          userAssigned: null
+        }
+      ]
+    }
+  ]
+}
