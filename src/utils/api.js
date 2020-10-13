945df9ef2ed8abf1045f0a06981967b74be6ecd3
Nazli Bozoglu <nzlbozoglu@gmail.com>
2020-10-13 19:31:42 -0400

    more contentful content

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,23 @@
 import { createClient } from 'contentful'
+console.log(process.env);
 
 export default createClient({
-  space: `5bto7dnjw78h`,
-  accessToken: `ea66ed0940d33a41147a4f9614954611360da770e240e8222d5adcdbe6ab56c6`
+  space: process.env.VUE_APP_CONTENTFUL_SPACE,
+  accessToken: process.env.VUE_APP_CONTENTFUL_ACCESS_TOKEN
+})
+var contentful = require('contentful')
+
+var client = contentful.createClient({
+  space: process.env.VUE_APP_CONTENTFUL_SPACE,
+  accessToken: process.env.VUE_APP_CONTENTFUL_ACCESS_TOKEN
+})
+
+client.getEntries()
+.then(function (entries) {
+  // log the title for all the entries that have it
+  entries.items.forEach(function (entry) {
+    if(entry.fields.title) {
+      console.log(entry.fields.title)
+    }
+  })
 })
